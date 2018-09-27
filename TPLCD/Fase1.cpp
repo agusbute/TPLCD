bce3ae53728a6c9da0400dd54804ed80bb90e453
Defalt <Defalt@DEFALT-PC>
2018-09-27 18:30:37 -0300

    kasjgf

--- TPLCD/Fase1.cpp
+++ TPLCD/Fase1.cpp
@@ -1,3 +1,71 @@
 #include "stdafx.h"
 #include "Fase1.h"
 
+#define MY_LCD_DESCRIPTION "EDA LCD 2 B"
+using namespace std;
+
+Fase1::
+Fase1()
+{
+	disp_status = FT_OK;
+	if ((disp_status = FT_OpenEx(1, &disp_handler)) == FT_OK)
+	{
+		if (FT_SetBitMode(disp_handler, 0xFF, 1) != FT_OK)		//0xFF es la mascara, 1 es el modo
+		{			
+			cout << "Couldn't configure LCD" << endl;
+		}
+	}
+	else
+	{
+		 cout<< "Couldn't open LCD" << endl;
+	}
+}
+
+Fase1::
+~Fase1()
+{
+
+};
+
+bool Fase1::
+lcdInit()
+{
+	
+};
+
+bool Fase1::
+lcdWriteIR(unsigned char byte)
+{
+
+};
+
+bool Fase1::
+lcdWriteDR(unsigned char byte)
+{
+	unsigned char temp = byte & 0xF0;
+	temp = temp | 0x20;
+	lcdwriteNibble(&disp_handler, temp);
+	temp = ((byte & 0x0F) <);
+
+};
+
+bool Fase1::
+lcdWriteByte(unsigned char, unsigned char)
+{
+
+};
+
+bool Fase1::
+lcdWriteNibble(FT_HANDLE * disp, unsigned char nibble)
+{
+	unsigned char temp = nibble & 0xFE; // 11111110
+	FT_Write(disp, &temp, 1, bytesSent);
+	Sleep(1);
+	temp = nibble | 0x01; // 00000001
+	FT_Write(disp, &temp, 1, bytesSent);
+	Sleep(3);
+	temp = nibble & 0xFE; // 11111110
+	FT_Write(disp, &temp, 1, bytesSent);
+	Sleep(1);
+	return true;
+};
--- TPLCD/Fase1.h
+++ TPLCD/Fase1.h
@@ -1,5 +1,9 @@
 ﻿#pragma once
+
 #include "stdafx.h"
+
+#define FTD_H
+#define FTD2XX_EXPORTS
 #include "ftd2xx.h"
 
 #define PORT_P0 0
@@ -25,14 +29,15 @@
 #define DISPLAY_CONTROL				0x08
 
 #define FUNCTION_SET				0x20	
-#define DSP_LINES_TWO				(1<<LINE_BIT) //USED IN FUNCTION SET
 #define DSP_LINES_ONE				(0<<LINE_BIT) //USED IN FUNCTION SET
+#define DSP_LINES_TWO				(1<<LINE_BIT) //USED IN FUNCTION SET
 #define FONT_5X8					(0<<FONT_BIT) //USED IN FUNCTION SET
 #define FONT_5X10					(1<<FONT_BIT) //USED IN FUNCTION SET
 
 #define DISPLAY_ON_OFF_CONTROL		0x0F
 #define SET_ADDRESS(a,b)			(0x80 + (a) + (b) * 0x40)
 
+using namespace std;
 
 class Fase1
 {
@@ -41,9 +46,9 @@ public:
 	~Fase1();
 	bool lcdInit();
 	bool lcdWriteIR(unsigned char byte); 
-	bool lcdWriteDR(unsigned char  byte);
-	bool lcdWriteByte(unsigned char  byte, unsigned char  byte);
-	bool lcdWriteNibble(unsigned char  byte);
+	bool lcdWriteDR(unsigned char byte);
+	bool lcdWriteByte(unsigned char, unsigned char);
+	bool lcdWriteNibble(unsigned char byte);
 
 protected:
 	FT_STATUS disp_status; 
@@ -52,13 +57,5 @@ private:
 	FT_HANDLE disp_handler;
 	Fase1(Fase1 &disp); 
 	Fase1& operator= (const Fase1& disp); 
-
 };
 
-
-
-//Asignaci�n de los bits de un puerto. 
-
-
-
-#endif // LOWLCD_H
